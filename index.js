1b52a692c7bffc01a172958c2f6f3a7b72993d07
Cesar <cdelgadocerna@gmail.com>
2018-07-10 23:14:34 -0500

    v0.0.1

---
+++ index.js
@@ -0,0 +1,24 @@
+//Modulos necesarios
+const express = require("express"); //Express: Framework web (Similar a Django)
+const users = require("./routes/users.js"); //Acceso a las rutas /usuario
+const ranking = require("./routes/ranking.js"); //Acceso a las rutas /ranking
+
+//Configuracion
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+
+//Routes
+app.use("/usuario", users);
+app.use("/ranking", ranking);
+
+//Documentacion API
+app.get("/apidoc", function (req, res) {
+    res.sendFile(__dirname + "/apidoc.html");
+});
+
+//Puerto del servidor
+app.listen(8080);
---
+++ models/ranking.js
@@ -0,0 +1,57 @@
+const Sequelize = require('sequelize');
+
+var sequelize = new Sequelize(null, null, null, {
+    dialect: "sqlite",
+    storage: './db.sqlite',
+    operatorsAliases: false,
+    logging: false,
+    define: {
+        freezeTableName: true,
+        timestamps: false
+    }
+});
+
+var usersTable = sequelize.define('users', {
+    username: {
+        type: Sequelize.TEXT,
+        primaryKey: true
+    },
+    password: Sequelize.TEXT,
+    score: Sequelize.INTEGER
+});
+
+class Ranking {
+    read(place) {
+        return sequelize.sync().then(function () {
+            return usersTable.findAll({
+                attributes: ["username", "score"],
+                order: [
+                    ["score", "DESC"]
+                ],
+                limit: 10
+            }).then(function (users) {
+                if (place == undefined) {
+                    return users;
+                } else {
+                    for (let i = 0; i < users.length; i++) {
+                        return users[place - 1];
+                    }
+                }
+            });
+        });
+    }
+
+    find(username) {
+        return this.read().then(function (users) {
+            for (let i = 0; i < users.length; i++) {
+                if (users[i].username == username) {
+                    return {
+                        pos: i + 1
+                    }
+                }
+            }
+        });
+    }
+}
+
+module.exports = Ranking;
